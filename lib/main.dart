0fd74313bd4b22cac3fd5dfd74d5105511260ed4
Ebrahim22Hassan <110447959+Ebrahim22Hassan@users.noreply.github.com>
2023-08-28 19:38:29 +0800

    register adapter and open contact box

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_assessment/features/home/data/models/contact_model.dart';
 import 'package:flutter_assessment/features/home/presentation/manager/contacts_cubit/contacts_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'constants.dart';
 import 'core/utils/app_router.dart';
 import 'core/utils/bloc_observer.dart';
 import 'core/utils/service_locator.dart';
 import 'features/home/data/repos/home_repo_implementation.dart';
 
-void main() {
+void main() async {
   setupServiceLocator();
   Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
+
+  Hive.registerAdapter(ContactModelAdapter());
+  await Hive.openBox(kContactBox);
 }
 
 class MyApp extends StatelessWidget {
